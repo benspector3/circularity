234c82eb21b58e28575a4946b942fc15e1258c6a
Ben Spector <ben@operationspark.org>
2018-07-31 20:32:43 +0000

    circularity debugging assessment try 1

--- js/init.js
+++ js/init.js
@@ -14,15 +14,31 @@ var init = function (window) {
     ////////////////////////////////////////////////////////////////
     
     // TODO 1 : Declare and initialize our variables //
-    
+    var circle;
+    var circles = [];
     // TODO 2 : Create a function that draws a circle  //
-    var drawCircle;
+    var drawCircle = function() {
+        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+        physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle);
+        circle.push(circle);
+        
+        
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+    };
     
     
-    // TODO 3 : Call the drawCircle function 3 times //
+    // TODO 3 : Call the drawCircle function 5 times //
 
     // TODO 7 : Create a Loop to call drawCircle 100 times
-
+    var counter = 0;
+    while (counter < 10);
+        drawCircle();
+        counter++;
 
     view.addChild(fps);
     app.addUpdateable(fps);
@@ -31,22 +47,37 @@ var init = function (window) {
         // TODO 5 : YOUR CODE STARTS HERE //////////////////////
         if ( circle.x > canvas.width + circle.radius ) {
             circle.x = 0 - circle.radius;
-        } else if ( / * test for left-side * / ) {
-            // your code to place circle exactly off the stage at the right-side //
-        } if ( / * test for top * / ) {
-            // code to place circle exactly off the stage at the bottom //
-        } else if ( / * test for bottom * / ) {
-            // your code to place circle exactly off the stage at the top //
+        } else if ( circle.x < canvas.width + circle.radius ) {
+           circle.x = 0 + circle.radius;
+        } if ( circle.y > canvas.height + circle.radius ) {
+            circle.y = 0 - circle.radius;
+        } else if ( circle.y < canvas.heigth + circle.radius ) {
+            circle.y = 0 + circle.radius;
         }
         // YOUR TODO 5 CODE ENDS HERE //////////////////////////
     }
 
     function update() {
         // TODO 4 : Update the circle's position //
-
+        physikz.updatePosition(circles[0]);
+        physikz.updatePosition(circles[0]);
+        physikz.updatePosition(circles[0]);
+        physikz.updatePosition(circles[0]);
+        physikz.updatePosition(circles[0]);
         // TODO 6 : Call checkCircleBounds on your circles.
-
+        checkCircleBounds(circles[1]);
+        checkCircleBounds(circles[2]);
+        checkCircleBounds(circles[3]);
+        checkCircleBounds(circles[4]);
+        checkCircleBounds(circles[5]);
         // TODO 8 : Iterate over the array
+        /*
+        for (var i = 0; i < circles.length - 1; i++) {
+            var circle = circles[0];
+            physikz.updatePosition(circle);
+            checkCircleBounds(circle);
+        }
+        */
     }
         
     ////////////////////////////////////////////////////////////////////
